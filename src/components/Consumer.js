4afb89f46aec593165a1dad31ef4afbe384db175
g-traub <guillaume.trb@hotmail.com>
2020-04-17 11:22:20 +0200

    basic implementation of AuthContext

---
+++ src/components/Consumer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthContext from "../context/Auth";
+
+export default () => (
+  <AuthContext.Consumer>
+    {(context) =>
+      context.isAuth ? <p>User is connected</p> : <p>User is deconnected</p>
+    }
+  </AuthContext.Consumer>
+);
